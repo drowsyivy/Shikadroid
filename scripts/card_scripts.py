1fbdd7d8bff030811abf76b801d991bea6c7868d
MageKing17 <MageKing17@gmail.com>
2019-10-28 00:57:14 -0700

    Decouple parsing and formatting, add regexp-based parsing.
    Changes parse_*() to format_*() (since they format internal deck representations into user-viewable strings) and adds replacement parse_*() functions that are intended to actually validate arbitrary user input. They use regexps and do basic sanity checks to ensure that input is sensible; if it is not, they raise a ParseError which can be caught by the calling code to, say, pass it along to the user so they know why it didn't work.

--- scripts/card_scripts.py
+++ scripts/card_scripts.py
@@ -1,6 +1,141 @@
 #!/usr/bin/env python3.8
 
 import random
+import re
+
+
+HANAFUDA_CARD = re.compile(
+    # Month
+    r"\b(?:(?P<suit>1[0-2]|[1-9]|pine|january|plum|february|cherry|march"
+    r"|wisteria|april|iris|may|peony|june|lespedeza|july|pampas|august"
+    r"|chrysanthemum|september|maple|october|willow|november|paulownia"
+    r"|december) ?)?"
+    # Value
+    r"(?P<value>[xtpq]|plain|flower"
+    r"|(?:(?:red )?(?:poem )?|blue )?(?:tanzaku|ribbon)|tane|animal|light"
+    r"|crane|nightingale|curtain|cuckoo|bridge|butterflies|boar|moon|geese"
+    r"|sake cup|deer|rain|swallow|lightning|phoenix)"
+    # Reversed order month
+    r"(?(suit)|(?: of | )?(?P<suit>1[0-2]|[1-9]|pine|january|plum|february"
+    r"|cherry|march|wisteria|april|iris|may|peony|june|lespedeza|july|pampas"
+    r"|august|chrysanthemum|september|maple|october|willow|november|paulownia"
+    r"|december)?)\b",
+    re.IGNORECASE
+)
+HANAFUDA_VALUES = {
+    "plain":            "x",
+    "flower":           "x",
+    "tanzaku":          "t",
+    "ribbon":           "t",
+    "red poem tanzaku": "t",
+    "red poem ribbon":  "t",
+    "poem tanzaku":     "t",
+    "poem tanzaku":     "t",
+    "red tanzaku":      "t",
+    "red ribbon":       "t",
+    "blue tanzaku":     "t",
+    "blue ribbon":      "t",
+    "tane":             "p",
+    "animal":           "p",
+    "light":            "q"
+}
+HANAFUDA_SUITS = {
+    "pine":             "1",
+    "january":          "1",
+    "plum":             "2",
+    "february":         "2",
+    "cherry":           "3",
+    "march":            "3",
+    "wisteria":         "4",
+    "april":            "4",
+    "iris":             "5",
+    "may":              "5",
+    "peony":            "6",
+    "june":             "6",
+    "lespedeza":        "7",
+    "july":             "7",
+    "pampas":           "8",
+    "august":           "8",
+    "chrysanthemum":    "9",
+    "september":        "9",
+    "maple":            "10",
+    "october":          "10",
+    "willow":           "11",
+    "november":         "11",
+    "paulownia":        "12",
+    "december":         "12"
+}
+HANAFUDA_UNIQUES = {
+    "crane":        "q1",
+    "nightingale":  "p2",
+    "curtain":      "q3",
+    "cuckoo":       "p4",
+    "bridge":       "p5",
+    "butterflies":  "p6",
+    "boar":         "p7",
+    "geese":        "p8",
+    "moon":         "q8",
+    "sake cup":     "p9",
+    "deer":         "p10",
+    "lightning":    "x11",
+    "swallow":      "p11",
+    "rain":         "q11",
+    "phoenix":      "q12"
+}
+HANAFUDA_MONTHS = {
+    "1": "Pine",
+    "2": "Plum",
+    "3": "Cherry",
+    "4": "Wisteria",
+    "5": "Iris",
+    "6": "Peony",
+    "7": "Lespedeza",
+    "8": "Pampas",
+    "9": "Chrysanthemum",
+    "10": "Maple",
+    "11": "Willow",
+    "12": "Paulownia"
+}
+
+CARD = re.compile(
+    # Suit
+    r"\b(?:(?P<suit>[rgbyxscdh]|red|green|blue|yellow|spades|clubs|diamonds"
+    r"|hearts) ?)?"
+    # Value
+    r"(?P<value>10|[0-9srwtfd#akqj]|one|two|three|four|five|six|seven|eight"
+    r"|nine|ten|skip|reverse|wild(?: draw four| dos| #)?|draw two|draw four"
+    r"|dos|ace|king|queen|jack|joker)"
+    # Reversed order suit
+    r"(?(suit)|(?: of | )?(?P<suit>[rgbyxscdh]|red|green|blue|yellow|spades"
+    r"|clubs|diamonds|hearts)?)\b",
+    re.IGNORECASE
+)
+VALUES = {
+    "zero":             "0",
+    "one":              "1",
+    "two":              "2",
+    "three":            "3",
+    "four":             "4",
+    "five":             "5",
+    "six":              "6",
+    "seven":            "7",
+    "eight":            "8",
+    "nine":             "9",
+    "ten":              "10",
+    "skip":             "s",
+    "wild":             "w",
+    "draw two":         "t",
+    "wild draw four":   "f",
+    "draw four":        "f",
+    "wild dos":         "d",
+    "dos":              "d",
+    "wild #":           "#",
+    "ace":              "a",
+    "king":             "k",
+    "queen":            "q",
+    "jack":             "j",
+    "joker":            "j"
+}
 
 
 def gen_uno_deck(count: int = 1, suits: str = "rgby", colourless: int = None):
@@ -62,25 +197,72 @@ class ParseError(ValueError):
     pass
 
 
-HANAFUDA_MONTHS = {
-    "1": "Pine",
-    "2": "Plum",
-    "3": "Cherry",
-    "4": "Wisteria",
-    "5": "Iris",
-    "6": "Peony",
-    "7": "Lespedeza",
-    "8": "Pampas",
-    "9": "Chrysanthemum",
-    "10": "Maple",
-    "11": "Willow",
-    "12": "Paulownia"
-}
-
-
 def parse_hanafuda(card: str):
+    match = HANAFUDA_CARD.match(card)
+    if not match:
+        raise ParseError(f"Invalid Hanafuda card {card!r}; must be a month and a type, or the name of a special card.")
+    value, suit = match.group("value", "suit")
+    value, suit = value.casefold(), suit.casefold()
+    if not suit:
+        if value in HANAFUDA_UNIQUES:   # Unique card names (don't need month)
+            return HANAFUDA_UNIQUES[value]
+        else:
+            raise ParseError(f"Monthless Hanafuda card {card!r} isn't unique.")
+    if suit in HANAFUDA_SUITS:      # Translate words into number( string)s
+        suit = HANAFUDA_SUITS[suit]
+    elif suit not in HANAFUDA_MONTHS:
+        raise ParseError(f"Invalid Hanafuda suit {suit!r} in card {card!r}.")
+    if value in HANAFUDA_UNIQUES:
+        compare = HANAFUDA_UNIQUES[value]
+        if compare[1:] == suit:
+            return compare
+        else:
+            raise ParseError(f"Unique Hanafuda card name {value!r} can't be found in month {suit!r}; card {card!r} is invalid.")
+    if ((value in ("red poem tanzaku", "poem tanzaku", "red poem ribbon", "poem ribbon") and suit not in "123") or
+            (value in ("red tanzaku", "red ribbon") and suit not in ("4", "5", "7", "11")) or
+            (value in ("blue tanzaku", "blue ribbon") and suit not in ("6", "9", "10"))):
+        raise ParseError(f"Hanafuda cards of type {value!r} can't be found in month {suit!r}; card {card!r} is invalid.")
+    if value in HANAFUDA_VALUES:    # Translate words into x, t, p, or q
+        value = HANAFUDA_VALUES[value]
+    elif value not in "xtpq":
+        raise ParseError(f"Invalid Hanafuda value {value!r} in card {card!r}.")
+    if ((value == "q" and suit not in (1, 3, 8, 11, 12)) or
+            (value == "p" and suit in (1, 3, 12)) or
+            (value == "t" and suit in (8, 12))):
+        raise ParseError(f"Hanafuda value {value!r} can't be found in month {suit!r}; card {card!r} is invalid.")
+    return "".join((value, suit))
+
+
+def parse_card(card: str):
+    match = CARD.match(card)
+    if not match:
+        raise ParseError(f"Invalid card {card!r}; must be a suit followed by a value.")
+    suit, value = match.group("suit", "value")
+    suit, value = suit.casefold(), value.casefold()
+    if not suit:
+        suit = "x"
+    else:
+        if suit[0] not in "rgbyxscdh":
+            raise ParseError(f"Invalid suit {suit!r} in card {card!r}.")
+        else:
+            suit = suit[0]
+    if value == "joker" and not suit == "x":
+        raise ParseError(f"Jokers can't have suits (suit {suit!r} in card {card!r}).")
+    if value in VALUES:
+        value = VALUES[value]
+    elif value not in "srwtfd#akqj" and not (value.isdigit() and 0 <= int(value) <= 10):
+        # This shouldn't be possible unless CARD or VALUES are wrong.
+        raise AssertionError(f"Invalid value {value!r} in card {card!r}; either the regexp or the value dict are incorrect.")
+    if suit in "scdh" and value in "01swtfd#":
+        raise ParseError(f"Nonsensical suit/value pair ({suit}/{value}) in card {card!r}.")
+    elif suit in "rgby" and value in "akqj":
+        raise ParseError(f"Nonsensical suit/value pair ({suit}/{value}) in card {card!r}.")
+    return "".join((suit, value))
+
+
+def format_hanafuda(card: str):
     if not len(card) == 2 and not card[1:].isdigit():
-        raise ParseError(f"Invalid format {card!r}. Cards should be in format \"s#\".")
+        raise ValueError(f"Invalid format {card!r}. Cards should be in format \"s#\".")
 
     if card[0] == 'x':
         prefix = "```\n~"
@@ -89,13 +271,13 @@ def parse_hanafuda(card: str):
     elif card[0] in "pq":
         prefix = "```diff\n+"
     else:
-        raise ParseError(f"Invalid value {card[0]!r} in {card!r}. Valid values are 'x', 't', 'p' and 'q'.")
+        raise ValueError(f"Invalid value {card[0]!r} in {card!r}. Valid values are 'x', 't', 'p' and 'q'.")
 
     if card[1:] in HANAFUDA_MONTHS:
         month_en = HANAFUDA_MONTHS[card[1:]]
         month_num = int(card[1:])
     else:
-        raise ParseError(f"Invalid month {card[1:]!r} in {card!r}. Valid months are 1-12.")
+        raise ValueError(f"Invalid month {card[1:]!r} in {card!r}. Valid months are 1-12.")
 
     if card[0] == 'p':      # 10 pts
         try:
@@ -111,7 +293,7 @@ def parse_hanafuda(card: str):
                 11: "Rain"
             }[month_num]
         except KeyError:
-            raise ParseError(f"Card value 'p' invalid in month {month_num!r} ({month_en}).") from None
+            raise ValueError(f"Card value 'p' invalid in month {month_num!r} ({month_en}).") from None
     elif card[0] == 'q':    # 20 pts
         try:
             value_en = {
@@ -122,7 +304,7 @@ def parse_hanafuda(card: str):
                 12: "Phoenix"
             }[month_num]
         except KeyError:
-            raise ParseError(f"Card value 'q' invalid in month {month_num!r} ({month_en}).") from None
+            raise ValueError(f"Card value 'q' invalid in month {month_num!r} ({month_en}).") from None
     elif card[0] == 't':    # tanzaku
         if 1 <= month_num <= 3:
             value_en = "Red Poem Tanzaku"
@@ -131,7 +313,7 @@ def parse_hanafuda(card: str):
         elif month_num in (6, 9, 10):
             value_en = "Blue Tanzaku"
         else:
-            raise ParseError(f"Card value 't' invalid in month {month_num!r} ({month_en}).")
+            raise ValueError(f"Card value 't' invalid in month {month_num!r} ({month_en}).")
     elif card[0] == 'x':    # plain
         if month_num == 11:
             value_en = "Lightning"
@@ -147,9 +329,9 @@ def parse_hanafuda(card: str):
     return " ".join((prefix, month_en, value_en, suffix))
 
 
-def parse_card(card: str):
+def format_card(card: str):
     if not len(card) == 2 and not card[1:].isdigit():
-        raise ParseError(f"Invalid format {card!r}. Cards should be in format \"s#\".")
+        raise ValueError(f"Invalid format {card!r}. Cards should be in format \"s#\".")
 
     if card[0] == 'x':
         prefix = "```\n~"
@@ -170,7 +352,7 @@ def parse_card(card: str):
     elif card[0] == 'h':
         prefix = "```diff\n- \u2665"
     else:
-        raise ParseError(f"Invalid colour {card[0]!r} in {card!r}. Valid suits are 's', 'c', 'd', 'h', 'r', 'g', 'b', and 'y', and colourless 'x'.")
+        raise ValueError(f"Invalid colour {card[0]!r} in {card!r}. Valid suits are 's', 'c', 'd', 'h', 'r', 'g', 'b', and 'y', and colourless 'x'.")
 
     if card[1:].isdigit():
         value = card[1:]
@@ -197,7 +379,7 @@ def parse_card(card: str):
     elif card[1] == 'j':
         value = "Joker" if (card[0] == 'x') else "Jack"
     else:
-        raise ParseError(f"Invalid card {card[1:]!r} in {card!r}. Valid cards are 0-10, a, k, q, j, s, r, w, t, f, d, and #.")
+        raise ValueError(f"Invalid card {card[1:]!r} in {card!r}. Valid cards are 0-10, a, k, q, j, s, r, w, t, f, d, and #.")
 
     if card[0] == 'x':
         suffix = "~```"
